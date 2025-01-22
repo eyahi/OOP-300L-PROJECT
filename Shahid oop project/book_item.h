078260e973f43047ad769a1a23c93e126a72cb30
shahidaliyu27 <150688791+shahidaliyu27@users.noreply.github.com>
2025-01-22 14:13:28 +0100

    Add files via upload

---
+++ Shahid oop project/book_item.h
@@ -0,0 +1,28 @@
+#ifndef BOOK_ITEM_H
+#define BOOK_ITEM_H
+
+#include "library_item.h"
+
+class BookItem : public LibraryItem {
+private:
+    std::string author;
+    std::string isbn;
+
+public:
+    // Constructor
+    BookItem(const std::string& title, const std::string& author, const std::string& isbn);
+
+    // Getters
+    std::string getAuthor() const;
+    std::string getISBN() const;
+
+    // Setters
+    void setAuthor(const std::string& author);
+    void setISBN(const std::string& isbn);
+
+    // Overridden functions
+    void printDetails() const;
+    double calculateLateFees(int daysOverdue) const;
+};
+
+#endif // BOOK_ITEM_H
